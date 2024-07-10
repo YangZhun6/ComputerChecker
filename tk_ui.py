878100f0a7254546b1b787c711d6fdbd83b35bf6
zyhsck <zyhsxyx@outlook.com>
2024-07-10 16:12:03 +0800

    114514

--- tk_ui.py
+++ tk_ui.py
@@ -4,7 +4,8 @@ from MemoryINFO import *
 from motherboard import *
 
 def testcpu():
-    global Test_cpu_frame_text
+    global Test_frame_text
+    Test_frame_text.delete('1.0',"end")
     cpu_information=f"\
 -------CPU检测结果------\n\
 cpu_max_freq:{cpu_max_freq()}\n\
@@ -15,8 +16,38 @@ all_cpu_usage:{all_cpu_usage()}\n\
 cpu_usage:{cpu_usage(1)}\n\
 cpuid:{cpuid()}\n\
 -----------------------"
-    Test_cpu_frame_text.insert('end',cpu_information)
+    Test_frame_text.insert('end',cpu_information)
     print(cpu_information)
+
+def testmemory():
+    global Test_frame_text
+    Test_frame_text.delete('1.0',"end")
+    memory_information=f"\
+-------内存检测结果------\n\
+memory_total:{memory_total()}\
+memory_available:{memory_available()}\
+memory_used:{memory_used()}\
+memory_usage:{memory_usage()}\
+max_memory_capacity:{get_max_memory_capacity()}\
+memory_speeds:{memory_speeds()}\
+memory_sizes:{memory_sizes()}\
+memory_num:{memory_num()}\
+-----------------------"
+    Test_frame_text.insert('end',memory_information)
+    print(memory_information)
+
+def testmotherboard():
+    global Test_frame_text
+    Test_frame_text.delete('1.0',"end")
+    motherboard_information=f"\
+-------主板检测结果------\n\
+:{motherboard_vendor()}\n\
+:{Hardware.get_baseboard_sn()}\n\
+:{Hardware.get_bios_sn()}\n\
+-----------------------"
+    Test_frame_text.insert('end',motherboard_information)
+    print(motherboard_information)
+
 
 
 root = Tk()
@@ -32,6 +63,10 @@ mouse_sp.pack()
 
 Test_cpu = Button(root,text="检测cpu",command=lambda:root.after(1,testcpu))
 Test_cpu.place(x=10,y=30)
+Test_memory = Button(root,text="检测内存",command=lambda:root.after(1,testmemory))
+Test_memory.place(x=80,y=30)
+Test_motherboard = Button(root,text="检测主板",command=lambda:root.after(1,testmotherboard))
+Test_motherboard.place(x=150,y=30)
 
-Test_cpu_frame_text = Text(root,height=25,width=35)
-Test_cpu_frame_text.place(x=10,y=60)
+Test_frame_text = Text(root,height=25,width=35)
+Test_frame_text.place(x=10,y=60)
